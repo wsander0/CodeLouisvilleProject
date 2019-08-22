33bc843b76c9c9de91c672ecdeb5f0e35eaf4bdf
Will Sanders <willsanders079@gmail.com>
2019-08-22 02:33:39 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,52 @@
+ Project Name: Everything you need to know about Luna.
+
+Description: This project consists of information about my dog Luna.  It goes through some of her favorite activites, and some information about her, and has some additional photos to look through. The main point of this project was to portray my skills and knowledge of basic front end development and while most things went as planned, I ran into some issues along the way.
+
+# Custom Css
+
+(.topnav, .topnavresponsive)-Styles the topnav and the responsive topnav.
+
+(.nodisp)-This class was added to all no active pages, so when the user is on one of the pages it is highlighted and prevents the others from acting as if they are on that page.
+
+    /** Home Page **/
+
+(.title)-Style the home page title.
+
+(.bg-img #bgimg2)-Adds styles to the background home page images, and adds the responsive change to the smaller img.
+
+
+
+    /** About Page **/
+
+(.flex-container)-Used as a container for the flex box used to style the div.
+
+(.lastabt)-Used to style the 2nd to last p because the additional text.
+
+    /** Activities Page **/
+
+(.row1 and .row2)-Used to style the 2 rows
+
+(.act-column and .act1-6)-Used to style the columns and each of the activities if needed.
+
+    /** Photo Gallery Page **/
+
+(.row .column .demo-cursor)-Used to style the rows and columns as well as the images to set up the js and images. 
+
+    /** Contact Page **/
+
+(#icons)-Used to add the roate effect to the icons when focused on.  
+
+
+
+
+# Custom JS
+
+Added some js to add an additional class of "responsive" to the responsive menu when the icon is clicked.
+
+Added some js for the photo gallery. Had trouble getting this to do exactly what I wanted it to do but thought it was a neat feature to have added.
+
+Added a function to work with the button that you can click when you scroll down to take you back to the top of the page.
+
+Attempted to create a function that scrolled the user to the top of the page once they moved 20px down the page but I wasnt able to get it to work exactly how I wanted.  I will revisit this.
+
+Added some js to manipulate the contact icons when focused on.
---
+++ main.js
@@ -0,0 +1,97 @@
+    /** Topnav JS **/
+
+/** Toggle adding and removing class responsive to topnav when user clicks icon **/
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += "responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+
+/** Failed attempt at using a Top Scroll Button.  Will revisit **/
+
+
+
+//    /** Adding Button Function **/
+//
+///** When the user scrolls 20px from top the button will appear **/
+//
+//window.onscoll = function() {scrollFunction()};
+//
+//function scrollFunction() {
+//    if (document.body.scrollTop > 20 || document.documentElement.scrollTop >20) {
+//        document.getElementById("myBtn").style.display = "block";
+//    } else {
+//        document.getElementById("myBtn").style.display = "none";
+//    }
+//}
+//
+///** When the user clicks the button it will take them to the top of the page (For safari and Chrome, Firefox etc...**/
+//
+//function topFunction() {
+//    document.body.scrollTop = 0;
+//    document.documentElement.scrollTop = 0;
+//}
+
+
+
+
+    /** Adding slide interaction **/
+
+    var slideIndex = 1;
+        showSlides(slideIndex);
+
+    /** Next/previous controls **/
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+}
+
+    /** Thumbnail image controls **/
+
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+}
+
+    function showSlides(n) {
+        var i;
+        var slides = document.getElementsByClassName("mySlides");
+        var dots = document.getElementsByClassName("demo");
+        var captionText = document.getElementById("caption");
+            if (n > slides.length) {slideIndex = 1}
+            if (n < 1) {slideIndex = slides.length}
+                for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+}
+                for (i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace(" active", "");
+}
+        slides[slideIndex-1].style.display="block";
+        dots[slideIndex-1].className += " active";
+        captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
